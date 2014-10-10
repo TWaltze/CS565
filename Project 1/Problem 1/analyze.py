3546c6a013eebe20d80471cba58c6d2162326b76
TWaltze <tylerwaltze@gmail.com>
2014-10-10 00:53:28 -0400

    Preprocess all data and basic predictions
    * Functions to preprocess all data (clean and structure as json)
    * Basic analyzing of data to make predictions

---
+++ Project 1/Problem 1/analyze.py
@@ -0,0 +1,118 @@
+import json
+
+def loadAuthors(input_file = "features_json.txt"):
+	with open(input_file, "r") as encoded:
+		authors = json.loads(encoded.read())
+
+		return authors
+
+def loadEdges(input_file = "edges_names_json.txt"):
+	with open(input_file, "r") as encoded:
+		edges = json.loads(encoded.read())
+
+		return edges
+
+def loadTraining(input_file = "train_json.txt"):
+	with open(input_file, "r") as encoded:
+		coauthors = json.loads(encoded.read())
+
+		return coauthors
+
+def generatePrediction(output_file = "prediction.csv"):
+	with open(output_file, "w") as output:
+		# Generate csv headers
+		output.write("id,coauthors\n")
+
+		# Generate coauthorship predictions
+		predictions = analyzeEdges()
+
+		for prediction in predictions:
+			output.write("{},{}\n".format(prediction, predictions[prediction]))
+
+def compareAuthors(author1, author2, authors = None):
+	if authors == None:
+		authors = loadAuthors()
+
+	author1 = authors[author1]
+	author2 = authors[author2]
+
+	shared = []
+	total = len(author1)
+
+	for key in author1:
+		if key in author2:
+			if not key in shared:
+				shared.append(key)
+
+	for key in author2:
+		if key in author1:
+			if not key in shared:
+				shared.append(key)
+		else:
+			# If a new term that isn't in author1
+			# is found, increase the total count
+			# of terms.
+			total += 1
+
+	'''
+	print("Author 1 terms: {}".format(len(author1)))
+	print("Author 2 terms: {}\n".format(len(author2)))
+
+	print("Shared terms: {} ({}%)".format(len(shared), (len(shared) / total * 100)))
+	'''
+
+	return {
+		"author1": len(author1),
+		"author2": len(author2),
+		"shared": len(shared) / total * 100
+	}
+
+def analyzeEdge(edge_id, edges = None, authors = None):
+	if edges == None:
+		edges = loadEdges()
+
+	edge = edges[str(edge_id)]
+	author1 = edge[0]
+	author2 = edge[1]
+
+	return compareAuthors(author1, author2, authors)
+
+def analyzeEdges():
+	edges = loadEdges()
+	authors = loadAuthors()
+	# coauthors = loadTraining()
+
+	prediction = {}
+	correct = 0
+	incorrect = 0
+	for edge in edges:
+		# Don't try to predict the first 5000 edges.
+		# We were given them already to test against.
+		if int(edge) > 5000:
+			data = analyzeEdge(edge, edges, authors)
+
+			# Basic prediction.
+			# If authors share more than
+			# 10% of the total terms between
+			# them, they are coauthors
+			if data["shared"] >= 10:
+				coauthored = 1
+			else:
+				coauthored = 0
+
+			prediction[edge] = coauthored
+
+			# Compare prediction to training
+			# file with correct answer
+			'''
+			if edges[edge] == coauthored:
+				correct += 1
+			else:
+				incorrect += 1
+			'''
+
+		# print("Correct: {}".format(correct))
+		# print("Incorrect: {}".format(incorrect))
+
+	return prediction
+
--- Project 1/Problem 1/preprocess.py
+++ Project 1/Problem 1/preprocess.py
@@ -7,51 +7,111 @@
 import re
 import json
 
-input_file = "features.txt"
-output_file = "_json.txt"
+#input_file = "features.txt"
+#output_file = "features_json.txt"
 
-processed = {}
-with open(input_file, "r") as raw:
-	for line in raw:
-		# Remove extra "," characters
-		clean = line.replace(",", "")
-		clean = clean.replace("  ", " ")
+def features(input_file = "features.txt", output_file = "features_json.txt"):
+	processed = {}
+	with open(input_file, "r") as raw:
+		for line in raw:
+			# Remove extra "," characters
+			clean = line.replace(",", "")
+			clean = clean.replace("  ", " ")
 
-		# Break string into words
-		words = clean.split(" ")
-		# First word is always author
-		author = words.pop(0)
-		terms = {}
+			# Break string into words
+			words = clean.split(" ")
+			# First word is always author
+			author = words.pop(0)
+			terms = {}
 
-		for word in words:
-			term, freq = word.split(":", 1)
+			for word in words:
+				term, freq = word.split(":", 1)
 
-			# Normalize term formatting
-			term = term.lower()
-			term = re.sub(r"[^a-z0-9]", "", term)
+				# Normalize term formatting
+				term = term.lower()
+				term = re.sub(r"[^a-z0-9]", "", term)
 
-			# Clean frequency
-			freq = re.sub(r"[^0-9]", "", freq)
-			freq = int(freq)
+				# Clean frequency
+				freq = re.sub(r"[^0-9]", "", freq)
+				freq = int(freq)
 
-			# Check for empty strings
-			if term:
-				if term in terms:
-					# Increment if duplicate term
-					terms[term] += freq
-				else:
-					# Add term to terms list
-					terms[term] = freq
+				# Check for empty strings
+				if term:
+					if term in terms:
+						# Increment if duplicate term
+						terms[term] += freq
+					else:
+						# Add term to terms list
+						terms[term] = freq
 
-		# Add to list of authors
-		processed[author] = terms
+			# Add to list of authors
+			processed[author] = terms
 
-# parsed = json.loads(processed)
-# print(json.dumps(processed, indent=4, sort_keys=True))
+	# parsed = json.loads(processed)
+	# print(json.dumps(processed, indent=4, sort_keys=True))
 
-# Encode as json for writing to new file
-encoded = json.dumps(processed)
+	# Encode as json for writing to new file
+	encoded = json.dumps(processed)
 
-# Write to new file
-with open(output_file, "w") as output:
-	output.write(encoded)
+	# Write to new file
+	with open(output_file, "w") as output:
+		output.write(encoded)
+
+def edges(input_file = "edges_names.csv", output_file = "edges_names_json.txt"):
+	processed = {}
+	with open(input_file, "r") as raw:
+		# Skip first line, contains column names
+		next(raw)
+
+		for line in raw:
+			# Remove extra "," characters
+			clean = line.replace(",", "")
+			clean = clean.replace("  ", " ")
+			clean = clean.strip()
+
+			# Break string into column data
+			data = clean.split(" ")
+			# First word is an edge id
+			edge_id = int(data.pop(0))
+			# Second is first author
+			author1 = data.pop(0)
+			# Third is second author
+			author2 = data.pop(0)
+
+			# Add to list of edges.
+			processed[edge_id] = [author1, author2]
+
+	# Encode as json for writing to new file
+	encoded = json.dumps(processed)
+
+	# Write to new file
+	with open(output_file, "w") as output:
+		output.write(encoded)
+
+def train(input_file = "train.csv", output_file = "train_json.txt"):
+	processed = {}
+	with open(input_file, "r") as raw:
+		# Skip first line, contains column names
+		next(raw)
+
+		for line in raw:
+			# Remove newline character
+			clean = line.strip()
+
+			# Break string into column data
+			data = clean.split(",")
+			# First word is an edge id
+			edge_id = int(data.pop(0))
+			# Second is whether or not edge
+			# represents coauthors
+			coauthors = int(data.pop(0))
+
+			# Add to list of edges.
+			processed[edge_id] = coauthors
+
+	# Encode as json for writing to new file
+	encoded = json.dumps(processed)
+
+	# Write to new file
+	with open(output_file, "w") as output:
+		output.write(encoded)
