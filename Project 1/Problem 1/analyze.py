538dca86d0a48424c9826921381e483f3a31745c
TWaltze <tylerwaltze@gmail.com>
2014-10-10 01:21:03 -0400

    Check for common words
    When comparing authors for similarity, ignore common terms they may
    share

--- Project 1/Problem 1/analyze.py
+++ Project 1/Problem 1/analyze.py
@@ -37,22 +37,29 @@ def compareAuthors(author1, author2, authors = None):
 	author2 = authors[author2]
 
 	shared = []
-	total = len(author1)
+	total = 0
 
 	for key in author1:
-		if key in author2:
-			if not key in shared:
-				shared.append(key)
+		# Don't count common words
+		if not isCommonWord(key):
+			# Keep count of total terms found
+			total += 1
+
+			if key in author2:
+				if key not in shared:
+					shared.append(key)
 
 	for key in author2:
-		if key in author1:
-			if not key in shared:
-				shared.append(key)
-		else:
-			# If a new term that isn't in author1
-			# is found, increase the total count
-			# of terms.
-			total += 1
+		# Don't count common words
+		if not isCommonWord(key):
+			if key in author1:
+				if key not in shared:
+					shared.append(key)
+			else:
+				# If a new term that isn't in author1
+				# is found, increase the total count
+				# of terms.
+				total += 1
 
 	return {
 		"author1": len(author1),
@@ -87,7 +94,7 @@ def analyzeEdges():
 			# If authors share more than
 			# 10% of the total terms between
 			# them, they are coauthors
-			if data["shared"] >= 10:
+			if data["shared"] >= 4:
 				coauthored = 1
 			else:
 				coauthored = 0
@@ -96,3 +103,7 @@ def analyzeEdges():
 
 	return prediction
 
+def isCommonWord(word):
+	common_words = ["a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount", "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as", "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"]
+
+	return word in common_words
